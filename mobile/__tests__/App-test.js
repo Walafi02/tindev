bb204e070cad38e6763316b624a56689f60bb64d
Walafi02 <walafif@yahoo.com>
2019-12-21 18:31:57 -0300

    starting project

---
+++ mobile/__tests__/App-test.js
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+// Note: test renderer must be required after react-native.
+
+it('renders correctly', () => {
+  // renderer.create(<App />);
+});
---
+++ mobile/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Signin from '~/pages/Signin';
+
+export default function App() {
+  return <Signin />;
+}
